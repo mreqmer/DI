2620b76d5a48fa485a77ee1a64da22d61d804282
mreqmer <marta.requejo@iesnervion.es>
2024-10-30 14:36:51 +0100

    -

---
+++ Tema09/Tema09/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Tema09
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ Tema09/Tema09/ViewModels/EdadVM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema09.ViewModels
+{
+    internal class EdadVM : INotifyPropertyChanged
+    {
+        #region Atributos
+        private DateTime fechaNac;
+        private int edad;
+        #endregion
+
+        #region Propiedades Publicas
+        public DateTime FechaNac {
+            get { return fechaNac; }
+            set { fechaNac = value; edad = DateTime.Now.Year - value.Year; NotifyPropertyChanged("Edad"); }
+        }
+        public int Edad
+        {
+            get { return edad; }
+        }
+        #endregion
+
+
+        #region Notify
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+        #endregion
+    }
+}
